e090e80fb72d388a3c3b3815cafeed0c1ecf9906
mru2-loves-computers <waldi240574@gmail.com>
2026-02-03 18:58:36 +0000

    font added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold text-red-500">
         Hello world!
       </h1>
     </>
